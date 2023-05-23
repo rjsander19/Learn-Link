29361b12e733b6baf47260bdbc8e6aadc64eab52
rjsander <rjsander19@gmail.com>
2023-05-23 12:54:44 -0700

    firebase troubleshooting

--- src/firebase.js
+++ src/firebase.js
@@ -1,27 +1,27 @@
-import firebase from 'firebase/compat/app';
-import 'firebase/compat/auth';
-import { initializeApp } from 'firebase/app';
+// import firebase from 'firebase/compat/app';
+// import 'firebase/compat/auth';
+// import { initializeApp } from 'firebase/app';
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAgLf-pm9dTMV9lPmELs3Fzs7DKwiodQhc",
-    authDomain: "learnlink-f0746.firebaseapp.com",
-    projectId: "learnlink-f0746",
-    storageBucket: "learnlink-f0746.appspot.com",
-    messagingSenderId: "312768904603",
-    appId: "1:312768904603:web:7e8f850f2f750b32f9753e",
-    measurementId: "G-68HSY6RZTQ"
-  };
+// const firebaseConfig = {
+//     apiKey: "AIzaSyAgLf-pm9dTMV9lPmELs3Fzs7DKwiodQhc",
+//     authDomain: "learnlink-f0746.firebaseapp.com",
+//     projectId: "learnlink-f0746",
+//     storageBucket: "learnlink-f0746.appspot.com",
+//     messagingSenderId: "312768904603",
+//     appId: "1:312768904603:web:7e8f850f2f750b32f9753e",
+//     measurementId: "G-68HSY6RZTQ"
+//   };
 
-  const app = initializeApp(firebaseConfig);
-  const auth = app.auth();
-  const provider = new app.auth.GoogleAuthProvider();
+//   const app = initializeApp(firebaseConfig);
+//   const auth = app.auth();
+//   const provider = new app.auth.GoogleAuthProvider();
   
-  function login() {
-    return auth.signInWithPopup(provider);
-  }
+//   function login() {
+//     return auth.signInWithPopup(provider);
+//   }
   
-  function logout() {
-    return auth.signOut();
-  }
+//   function logout() {
+//     return auth.signOut();
+//   }
   
-  export { login, logout, auth };
+//   export { login, logout, auth };
