50b53440b142e6ac26336401211ecc558707cbd3
rjsander <rjsander19@gmail.com>
2023-05-23 13:26:50 -0700

    firebase troubleshooting

--- src/firebase.js
+++ src/firebase.js
@@ -1,32 +1,30 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyAgLf-pm9dTMV9lPmELs3Fzs7DKwiodQhc",
-    authDomain: "learnlink-f0746.firebaseapp.com",
-    projectId: "learnlink-f0746",
-    storageBucket: "learnlink-f0746.appspot.com",
-    messagingSenderId: "312768904603",
-    appId: "1:312768904603:web:7e8f850f2f750b32f9753e",
-    measurementId: "G-68HSY6RZTQ"
+    apiKey: "AIzaSyDUPoWgkY_npLIMj4K3lv6_ZZsCGEm07xY",
+    authDomain: "learnlink-904f8.firebaseapp.com",
+    projectId: "learnlink-904f8",
+    storageBucket: "learnlink-904f8.appspot.com",
+    messagingSenderId: "181613345494",
+    appId: "1:181613345494:web:ce093a6a22bbf802e5a777",
+    measurementId: "G-5FX9WMN4DF"
   };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-
+//activate firebase app
 firebase.initializeApp(firebaseConfig);
 
 const auth = firebase.auth();
 
 //set up provider
 const provider = new firebase.auth.GoogleAuthProvider();
+
 function login() {
     return auth.signInWithPopup(provider);
+
 }
 function logout() {
     return auth.signOut();
 }
+
 export { login, logout, auth };
